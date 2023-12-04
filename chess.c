f6871526486f75bfdf7b2505b4c3e36518309b52
ThoriumSeven <djcat211@gmail.com>
2023-12-04 22:01:52 +0800

    1. Made the position and move just like the normal chess. 2. Made the character with real chess icons(though pretty hard to see). 3. Bug fixes.

--- chess.c
+++ chess.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 // this program is at its very early stages..
 // only blacks pawn can be moved
 void displayBoard(int board[8][8]);
 void initializeBoard(int board[8][8]);
 void move(int board[8][8], int x1, int y1, int x2, int y2);
+int converter(int *x1, int *y1, int *x2, int *y2, char *pos, char *mov);
 int main(void) {
     int board[8][8];
-    int x1, y1, x2, y2;
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    char pos[5];
+    char mov[5];
     initializeBoard(board);
     while(1) {
+        printf("%i %i %i %i\n", x1, y1, x2, y2);
         displayBoard(board);
-        scanf("%i %i %i %i", &x1, &y1, &x2, &y2);
+        printf("Enter your move(ex. e4 e6): " );
+        scanf("%2s %2s", pos, mov);
+        while(converter(&x1, &y1, &x2, &y2, pos, mov) != 1) {
+            printf("Enter your move(ex. e4 e6): " );
+            scanf("%2s %2s", pos, mov);
+        }
         move(board, x1, y1, x2, y2);
         system("clear");
+
     }
     return 0;
 }
@@ -25,40 +37,40 @@ void displayBoard(int board[8][8]) {
                 printf(".");
             }
             else if(board[i][j] == 1) {
-                printf("K");
+                printf("♚");
             }
             else if(board[i][j] == 2) {
-                printf("Q");
+                printf("♛");
             }
             else if(board[i][j] == 3) {
-                printf("R");
+                printf("♜");
             }
             else if(board[i][j] == 4) {
-                printf("B");
+                printf("♝");
             }
             else if(board[i][j] == 5) {
-                printf("N");
+                printf("♞");
             }
             else if(board[i][j] == 6) {
-                printf("P");
+                printf("♟︎");
             }
             else if(board[i][j] == 7) {
-                printf("k");
+                printf("♔");
             }
             else if(board[i][j] == 8) {
-                printf("q");
+                printf("♕");
             }
             else if(board[i][j] == 9) {
-                printf("r");
+                printf("♖");
             }
             else if(board[i][j] == 10) {
-                printf("b");
+                printf("♗");
             }
             else if(board[i][j] == 11) {
-                printf("n");
+                printf("♘");
             }
             else if(board[i][j] == 12) {
-                printf("p");
+                printf("♙");
             }
             printf("  ");
         }
@@ -118,6 +130,546 @@ void move(int board[8][8], int x1, int y1, int x2, int y2) {
             else {
                 printf("Invalid\n");
             }
+            printf("yahoo\n");
         }
     }
+}
+int converter(int *x1, int *y1, int *x2, int *y2, char *pos, char *mov) {
+    if(strcmp(pos, "h1") == 0) {
+        *x1 = 0;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h2") == 0) {
+        *x1 = 1;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h3") == 0) {
+        *x1 = 2;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h4") == 0) {
+        *x1 = 3;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h5") == 0) {
+        *x1 = 4;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h6") == 0) {
+        *x1 = 5;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h7") == 0) {
+        *x1 = 6;
+        *y1 = 0;
+    }
+    else if(strcmp(pos, "h8") == 0) {
+        *x1 = 7;
+        *y1 = 0;
+    }
+    //
+    else if(strcmp(pos, "g1") == 0) {
+        *x1 = 0;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g2") == 0) {
+        *x1 = 1;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g3") == 0) {
+        *x1 = 2;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g4") == 0) {
+        *x1 = 3;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g5") == 0) {
+        *x1 = 4;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g6") == 0) {
+        *x1 = 5;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g7") == 0) {
+        *x1 = 6;
+        *y1 = 1;
+    }
+    else if(strcmp(pos, "g8") == 0) {
+        *x1 = 7;
+        *y1 = 1;
+    }
+    //
+    else if(strcmp(pos, "f1") == 0) {
+        *x1 = 0;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f2") == 0) {
+        *x1 = 1;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f3") == 0) {
+        *x1 = 2;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f4") == 0) {
+        *x1 = 3;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f5") == 0) {
+        *x1 = 4;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f6") == 0) {
+        *x1 = 5;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f7") == 0) {
+        *x1 = 6;
+        *y1 = 2;
+    }
+    else if(strcmp(pos, "f8") == 0) {
+        *x1 = 7;
+        *y1 = 2;
+    }
+    //
+    else if(strcmp(pos, "e1") == 0) {
+        *x1 = 0;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e2") == 0) {
+        *x1 = 1;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e3") == 0) {
+        *x1 = 2;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e4") == 0) {
+        *x1 = 3;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e5") == 0) {
+        *x1 = 4;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e6") == 0) {
+        *x1 = 5;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e7") == 0) {
+        *x1 = 6;
+        *y1 = 3;
+    }
+    else if(strcmp(pos, "e8") == 0) {
+        *x1 = 7;
+        *y1 = 3;
+    }
+    //
+    else if(strcmp(pos, "d1") == 0) {
+        *x1 = 0;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d2") == 0) {
+        *x1 = 1;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d3") == 0) {
+        *x1 = 2;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d4") == 0) {
+        *x1 = 3;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d5") == 0) {
+        *x1 = 4;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d6") == 0) {
+        *x1 = 5;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d7") == 0) {
+        *x1 = 6;
+        *y1 = 4;
+    }
+    else if(strcmp(pos, "d8") == 0) {
+        *x1 = 7;
+        *y1 = 4;
+    }
+    //
+    else if(strcmp(pos, "c1") == 0) {
+        *x1 = 0;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c2") == 0) {
+        *x1 = 1;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c3") == 0) {
+        *x1 = 2;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c4") == 0) {
+        *x1 = 3;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c5") == 0) {
+        *x1 = 4;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c6") == 0) {
+        *x1 = 5;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c7") == 0) {
+        *x1 = 6;
+        *y1 = 5;
+    }
+    else if(strcmp(pos, "c8") == 0) {
+        *x1 = 7;
+        *y1 = 5;
+    }
+    //
+    else if(strcmp(pos, "b1") == 0) {
+        *x1 = 0;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b2") == 0) {
+        *x1 = 1;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b3") == 0) {
+        *x1 = 2;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b4") == 0) {
+        *x1 = 3;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b5") == 0) {
+        *x1 = 4;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b6") == 0) {
+        *x1 = 5;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b7") == 0) {
+        *x1 = 6;
+        *y1 = 6;
+    }
+    else if(strcmp(pos, "b8") == 0) {
+        *x1 = 7;
+        *y1 = 6;
+    }
+    //
+    else if(strcmp(pos, "a1") == 0) {
+        *x1 = 0;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a2") == 0) {
+        *x1 = 1;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a3") == 0) {
+        *x1 = 2;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a4") == 0) {
+        *x1 = 3;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a5") == 0) {
+        *x1 = 4;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a6") == 0) {
+        *x1 = 5;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a7") == 0) {
+        *x1 = 6;
+        *y1 = 7;
+    }
+    else if(strcmp(pos, "a8") == 0) {
+        *x1 = 7;
+        *y1 = 7;
+    }
+    else {
+        printf("Invalid position!\n");
+        return 0;
+    }
+    //this is where the move converter is...
+    if(strcmp(mov, "h1") == 0) {
+        *x2 = 0;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h2") == 0) {
+        *x2 = 1;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h3") == 0) {
+        *x2 = 2;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h4") == 0) {
+        *x2 = 3;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h5") == 0) {
+        *x2 = 4;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h6") == 0) {
+        *x2 = 5;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h7") == 0) {
+        *x2 = 6;
+        *y2 = 0;
+    }
+    else if(strcmp(mov, "h8") == 0) {
+        *x2 = 7;
+        *y2 = 0;
+    }
+    //
+    else if(strcmp(mov, "g1") == 0) {
+        *x2 = 0;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g2") == 0) {
+        *x2 = 1;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g3") == 0) {
+        *x2 = 2;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g4") == 0) {
+        *x2 = 3;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g5") == 0) {
+        *x2 = 4;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g6") == 0) {
+        *x2 = 5;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g7") == 0) {
+        *x2 = 6;
+        *y2 = 1;
+    }
+    else if(strcmp(mov, "g8") == 0) {
+        *x2 = 7;
+        *y2 = 1;
+    }
+    //
+    else if(strcmp(mov, "f1") == 0) {
+        *x2 = 0;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f2") == 0) {
+        *x2 = 1;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f3") == 0) {
+        *x2 = 2;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f4") == 0) {
+        *x2 = 3;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f5") == 0) {
+        *x2 = 4;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f6") == 0) {
+        *x2 = 5;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f7") == 0) {
+        *x2 = 6;
+        *y2 = 2;
+    }
+    else if(strcmp(mov, "f8") == 0) {
+        *x2 = 7;
+        *y2 = 2;
+    }
+    //
+    else if(strcmp(mov, "e1") == 0) {
+        *x2 = 0;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e2") == 0) {
+        *x2 = 1;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e3") == 0) {
+        *x2 = 2;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e4") == 0) {
+        *x2 = 3;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e5") == 0) {
+        *x2 = 4;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e6") == 0) {
+        *x2 = 5;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e7") == 0) {
+        *x2 = 6;
+        *y2 = 3;
+    }
+    else if(strcmp(mov, "e8") == 0) {
+        *x2 = 7;
+        *y2 = 3;
+    }
+    //
+    else if(strcmp(mov, "d1") == 0) {
+        *x2 = 0;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d2") == 0) {
+        *x2 = 1;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d3") == 0) {
+        *x2 = 2;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d4") == 0) {
+        *x2 = 3;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d5") == 0) {
+        *x2 = 4;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d6") == 0) {
+        *x2 = 5;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d7") == 0) {
+        *x2 = 6;
+        *y2 = 4;
+    }
+    else if(strcmp(mov, "d8") == 0) {
+        *x2 = 7;
+        *y2 = 4;
+    }
+    //
+    else if(strcmp(mov, "c1") == 0) {
+        *x2 = 0;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c2") == 0) {
+        *x2 = 1;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c3") == 0) {
+        *x2 = 2;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c4") == 0) {
+        *x2 = 3;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c5") == 0) {
+        *x2 = 4;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c6") == 0) {
+        *x2 = 5;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c7") == 0) {
+        *x2 = 6;
+        *y2 = 5;
+    }
+    else if(strcmp(mov, "c8") == 0) {
+        *x2 = 7;
+        *y2 = 5;
+    }
+    //
+    else if(strcmp(mov, "b1") == 0) {
+        *x2 = 0;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b2") == 0) {
+        *x2 = 1;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b3") == 0) {
+        *x2 = 2;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b4") == 0) {
+        *x2 = 3;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b5") == 0) {
+        *x2 = 4;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b6") == 0) {
+        *x2 = 5;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b7") == 0) {
+        *x2 = 6;
+        *y2 = 6;
+    }
+    else if(strcmp(mov, "b8") == 0) {
+        *x2 = 7;
+        *y2 = 6;
+    }
+    //
+    else if(strcmp(mov, "a1") == 0) {
+        *x2 = 0;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a2") == 0) {
+        *x2 = 1;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a3") == 0) {
+        *x2 = 2;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a4") == 0) {
+        *x2 = 3;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a5") == 0) {
+        *x2 = 4;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a6") == 0) {
+        *x2 = 5;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a7") == 0) {
+        *x2 = 6;
+        *y2 = 7;
+    }
+    else if(strcmp(mov, "a8") == 0) {
+        *x2 = 7;
+        *y2 = 7;
+    }
+    else {
+        printf("Invalid position!\n");
+        return 0;
+    }
+    return 1;
+
 }
